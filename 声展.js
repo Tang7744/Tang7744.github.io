bbf4270225618da60e1615e7c8a64c32861805f2
Tang7744 <126841395+Tang7744@users.noreply.github.com>
2023-05-06 22:08:07 +0800

    Add files via upload

---
+++ 声展.js
@@ -0,0 +1,19 @@
+function a(){
+  var audio = document.getElementById('music');
+if(audio.paused){
+  audio.play();//audio.play();// 播放
+}
+else{
+  audio.pause();// 暂停
+}
+}
+
+function b(){
+  var audio = document.getElementById('music');
+if(audio.paused){
+  audio.play();//audio.play();// 播放
+}
+else{
+  audio.pause();// 暂停
+}
+}
